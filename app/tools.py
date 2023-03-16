a5eacc464821e6053dd0797d77d2607c6c9a407a
chick_0 <chick_0@ch1ck.xyz>
2023-03-17 01:44:58 +0900

    fix: Set align to center

--- app/tools.py
+++ app/tools.py
@@ -7,5 +7,7 @@ def get_user_block(user_id: int) -> str:
         id=user_id
     ).first()
 
-    return f'<img class="rounded-circle me-2" width="32" src="{get_avatar(user)}" alt="{user.username}의 아바타">' + \
-        f'<b>{user.username}</b><span class="text-muted">#{user.discriminator}</span>'
+    return '<div class="d-flex align-items-center">' + \
+        f'<img class="rounded-circle me-2" width="32" src="{get_avatar(user)}" alt="{user.username}의 아바타">' + \
+        f'<b>{user.username}</b><span class="text-muted">#{user.discriminator}</span>' + \
+        '</div>'
